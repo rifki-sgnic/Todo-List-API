75b1a75f96b943ca0c5e6ace8d9b59f1e078f3a7
Muhammad Rifki <mrifki028@gmail.com>
2023-03-12 15:35:23 +0700

    fix bugs

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const db = require("./models");
 const PORT = process.env.PORT || 3000;
-const cors = require('cors')
+const cors = require("cors");
+const https = require("https");
+const fs = require("fs");
 
 /* Middlewares */
 const isAuth = require("./middlewares/isAuth");
@@ -25,7 +27,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-app.use(cors({origin: true}))
+app.use(cors({ origin: true }));
 
 app.use("/", indexRouter);
 app.use("/register", registerRouter);
@@ -33,6 +35,23 @@ app.use("/login", loginRouter);
 app.use("/logout", isAuth, logoutRouter);
 app.use("/users", isAuth, usersRouter);
 app.use("/todos", isAuth, todosRouter);
+
+if (process.env.NODE_ENV === "production") {
+  const privateKey = fs.readFileSync(
+    "/etc/letsencrypt/live/13-213-0-190.nip.io/privkey.pem",
+    "utf-8"
+  );
+
+  const certificate = fs.readFileSync(
+    "/etc/letsencrypt/live/13-213-0-190.nip.io/cert.pem",
+    "utf-8"
+  );
+
+  const credentials = { key: privateKey, cert: certificate };
+
+  const httpsServer = https.createServer(credentials, app);
+  httpsServer.listen(PORT);
+}
 
 db.sequelize.sync().then(() => {
   app.listen(PORT, () => {
